a77efbb429a67a3c99b74f66b514bdf4a0c3ee27
V-Silpin <varun.mohanta323@gmail>
2025-09-27 03:24:47 +0530

    Created Sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,59 @@
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+function Sidebar() {
+    const drawerWidth = 240;
+
+    return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        <List>
+            <ListItem>
+                <ListItemText primary="Varun Mohanta" />
+            </ListItem>
+        </List>
+        {/* <Toolbar /> */}
+        <Divider />
+        <List>
+          {['About', 'Projects', 'Experiences'].map((text) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['About', 'Projects', 'Experiences'].map((text) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+    );
+}
+
+export default Sidebar;
