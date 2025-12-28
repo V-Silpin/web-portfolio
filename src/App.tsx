6ad6e3506048a7b1da9f637c280340901b1e9866
V-Silpin <varun.mohanta323@gmail>
2025-12-28 12:26:37 +0530

    Add Mainbox component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Sidebar from './components/Sidebar';
+import Mainbox from './components/Mainbox';
 
 function App() {
   return (
     <>
       <Sidebar />
+      <Mainbox />
     </>
   );
 }
